7cc58fa5e126f4760ad853345e59877167ed67a6
Chaeyoung02 <chaedud02@naver.com>
2023-11-19 01:24:21 +0900

    2884

---
+++ src/BaekJun/boj2884.java
@@ -0,0 +1,31 @@
+package BaekJun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj2884 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br =  new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int h = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+
+        if(m < 45){
+
+            h--;
+            m = 60 - 45 + m;
+            if(h < 0){
+                h = 23;
+            }
+            System.out.print(h + " " + m);
+
+        }else{
+            System.out.print(h + " " + (m-45));
+        }
+
+
+    }
+}
