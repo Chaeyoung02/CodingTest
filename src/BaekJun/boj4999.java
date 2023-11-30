4a17142b8ee4c81cca0ad15e6b222afde6492cda
Chaeyoung02 <chaedud02@naver.com>
2023-12-01 00:53:54 +0900

    4999

---
+++ src/BaekJun/boj4999.java
@@ -0,0 +1,26 @@
+package BaekJun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj4999 {
+
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String str = br.readLine();
+        String str2 = br.readLine();
+
+        if(str.length() >= str2.length()){
+            System.out.print("go");
+        }else {
+            System.out.print("no");
+        }
+
+
+
+    }
+
+}
