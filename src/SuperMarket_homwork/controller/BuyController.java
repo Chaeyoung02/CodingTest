4b36660b6bfbafb16f654c76796d0123b3b62500
김채영 <109122828+Chaeyoung02@users.noreply.github.com>
2024-06-20 17:13:42 +0900

    coding_test

---
+++ src/SuperMarket_homwork/controller/BuyController.java
@@ -0,0 +1,20 @@
+package com.gn.controller;
+
+import com.gn.model.dao.BuyDao;
+import com.gn.model.vo.Buy;
+
+public class BuyController {
+	public void insert(int id, int prod_no, int buy_amount) {
+		Buy b = new Buy();
+		b.setUser_no(id);
+		b.setProd_no(prod_no);
+		b.setBuy_amount(buy_amount);
+		int result = new BuyDao().insert(b);
+		if(result > 0) {
+			System.out.println("구매가 완료");
+		}else {
+			System.out.println("구매가 실패");
+		}
+	}
+
+}
