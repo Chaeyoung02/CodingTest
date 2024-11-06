f51dcdaeb25345dc4005ec6b591b1f56ff54f939
Chaeyoung <chaedud02@naver.com>
2024-11-06 17:54:14 +0900

    :tada: 11/06 [프로그래머스] 숨어있는 숫자의 덧셈(1)

---
+++ src/Programmers_test/hideNumSum.java
@@ -0,0 +1,15 @@
+package Programmers_test;
+
+public class hideNumSum {
+        public int solution(String my_string) {
+            int answer = 0;
+            my_string = my_string.replaceAll("[a-zA-Z]", "");
+            String[] str = my_string.split("");
+
+            for (String s : str) {
+                answer += Integer.parseInt(s);
+            }
+
+            return answer;
+        }
+}
