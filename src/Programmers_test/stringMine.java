24ebc482e90b80d0f2e26941de648d1d54ea1b0e
Chaeyoung <chaedud02@naver.com>
2024-11-26 21:54:31 +0900

    :tada: 11/26 [프로그래머스] 문자열 내 마음대로 정렬하기

---
+++ src/Programmers_test/stringMine.java
@@ -0,0 +1,16 @@
+package Programmers_test;
+
+import java.util.Arrays;
+
+public class stringMine {
+    public String[] solution(String[] strings, int n) {
+        Arrays.sort(strings, (s1, s2) -> {
+            if (s1.charAt(n) == s2.charAt(n)) {
+                return s1.compareTo(s2);
+            }
+            return s1.charAt(n) - s2.charAt(n);
+        });
+
+        return strings;
+    }
+}
