02566f2e1ee06b37c33e342af630be1404874e36
Chaeyoung <chaedud02@naver.com>
2024-12-10 15:51:49 +0900

    :tada: 12/08 [백준] 팰린드롬수

---
+++ src/BaekJun/boj1259.java
@@ -0,0 +1,32 @@
+package BaekJun;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class boj1259 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+
+        while(true){
+            String str = br.readLine();
+            boolean check = true;
+            for(int i = 0; i <str.length()/2; i++){
+                if(str.charAt(i) != str.charAt(str.length()-i-1)){
+                    check = false;
+                }
+            }
+            if(str.equals("0")){
+                break;
+            }
+
+            if(check){
+                System.out.println("yes");
+            }else {
+                System.out.println("no");
+            }
+        }
+    }
+}
