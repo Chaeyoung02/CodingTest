78a77f9d362e667058524d873ffa954b93386837
Chaeyoung <chaedud02@naver.com>
2024-12-10 15:53:02 +0900

    :tada: 12/10 [백준] 팰린드롬수

--- src/BaekJun/boj1259.java
+++ src/BaekJun/boj1259.java
@@ -3,7 +3,6 @@ package BaekJun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class boj1259 {
     public static void main(String[] args) throws IOException {
