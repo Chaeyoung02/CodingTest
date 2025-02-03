50047a36e88e37cbde633c16429e279f45f9d55c
김채영 <109122828+Chaeyoung02@users.noreply.github.com>
2025-02-03 10:12:56 +0900

    :tada: 02/03 [백준] 회사에 있는 사람

--- src/BaekJun/boj7785.java
+++ src/BaekJun/boj7785.java
@@ -8,14 +8,13 @@ public class boj7785 {
 
         for(int i = 0; i < n; i++){
             String name = sc.next();
-            String state = sc.next();
-						// 직원 이름과 출퇴근 상태를 위한 문자열 입력 받기
+            String state = sc.next();	
             if(map.containsKey(name)){
                 map.remove(name);
-                // 만약 기존 키값에 직원 이름이 있다면, 둘다 삭제 처리
+
             }else {
                 map.put(name, state);
-								// 없다면, 삽입
+
             }
         }
 
